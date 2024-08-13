4d213a0ea011cd8ae432e6b3f4636c9a8ddb1ce1
Randolph Sapp <rs@ti.com>
2024-08-13 13:38:19 -0500

    lightweight-offscreen: add a lot of error handling
    Add very verbose error handling in the event anything goes wrong. This
    is an example of a minimal application but it is also going to be used
    to test things in an automated environment.
    
    Remove all asserts.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- lightweight-offscreen.c
+++ lightweight-offscreen.c
@@ -1,7 +1,6 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES2/gl2.h>
-#include <assert.h>
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -55,8 +54,10 @@ void dump(ContextData *data)
 	free(buffer);
 }
 
-void draw(ContextData *data)
+bool draw(ContextData *data)
 {
+	GLenum glres = GL_NO_ERROR;
+
 	// clang-format off
 	GLfloat vVertices[] = {
 		0.0f, 0.5f, 0.0f,
@@ -65,36 +66,98 @@ void draw(ContextData *data)
 	};
 	// clang-format on
 
-	// Set the viewport
+	/* set the viewport */
 	glViewport(0, 0, data->width, data->height);
-	// Clear the color buffer
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to set viewport. GL error:", glres);
+		goto RenderExit;
+	}
+
+	/* clear the color buffer */
 	glClear(GL_COLOR_BUFFER_BIT);
-	// Use the program object
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to clear buffer. GL error:", glres);
+		goto RenderExit;
+	}
+
+	/* use the program object */
 	glUseProgram(data->program);
-	// Load the vertex data
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to use program. GL error:", glres);
+		goto RenderExit;
+	}
+
+	/* load the vertex data */
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
-	assert(glGetError() == GL_NO_ERROR);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to assign vertex attribute data. GL error:", glres);
+		goto RenderExit;
+	}
+
+	/* enable usage of the vertex data */
 	glEnableVertexAttribArray(0);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to use vertex attribute data. GL error:", glres);
+		goto RenderExit;
+	}
+
+	/* draw the arrays with the given vertex attributes */
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	eglSwapBuffers(data->display, data->surface);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to draw arrays. GL error:", glres);
+		goto RenderExit;
+	}
+
+RenderExit:
+	return (glres == GL_NO_ERROR);
 }
 
-GLuint setup_shader(ContextData *data, const char *shader_source,
-		    GLint shader_type)
+bool setup_shader(ContextData *data, const char *shader_source,
+		  GLint shader_type)
 {
+	GLenum glres;
+
 	GLuint shader = glCreateShader(shader_type);
-	assert(glGetError() == GL_NO_ERROR);
+	glres = glGetError();
+	if (shader == 0) {
+		printf("%s %x\n", "Failed to create shader. GL error:", glres);
+		goto ShaderExit;
+	}
 
 	glShaderSource(shader, 1, &shader_source, NULL);
-	assert(glGetError() == GL_NO_ERROR);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n",
+		       "Failed to create shader from source. GL error:", glres);
+		goto ShaderExitDelete;
+	}
 
 	glCompileShader(shader);
-	assert(glGetError() == GL_NO_ERROR);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to compile shader. GL error:", glres);
+		goto ShaderExitDelete;
+	}
 
 	glAttachShader(data->program, shader);
-	assert(glGetError() == GL_NO_ERROR);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to attach shader. GL error:", glres);
+		goto ShaderExitDelete;
+	}
 
-	return shader;
+ShaderExitDelete:
+	/* mark shader to be deleted, it will only be deleted once it is
+	 * detached */
+	glDeleteShader(shader);
+ShaderExit:
+	return (glres == GL_NO_ERROR);
 }
 
 bool egl_create_surface(ContextData *data)
@@ -151,67 +214,116 @@ bool egl_init(ContextData *data)
 	/* setup egl for surfaceless rendering */
 	data->display = eglGetPlatformDisplay(EGL_PLATFORM_SURFACELESS_MESA,
 					      EGL_DEFAULT_DISPLAY, NULL);
-	assert(data->display != NULL);
+	if (data->display == NULL)
+		return false;
 
 	return eglInitialize(data->display, NULL, NULL);
 }
 
 int32_t main(void)
 {
-	bool res;
+	bool res = true;
+	GLenum glres = GL_NO_ERROR;
 	ContextData data = { .width = 1920, .height = 1080 };
 
 	/* choose a display under the surfacless platform */
 	res = egl_init(&data);
-	assert(res);
+	if (!res) {
+		printf("%s %x\n",
+		       "Bad display parameter. EGL error:", eglGetError());
+		goto ExitDisplay;
+	}
 
 	/* pick a known universal config to play with */
 	res = egl_choose_config(&data);
-	assert(res);
+	if (!res) {
+		printf("%s %x\n", "Bad config. EGL error:", eglGetError());
+		goto ExitDisplay;
+	}
 
 	/* bind the gles api to this thread */
 	res = eglBindAPI(EGL_OPENGL_ES_API);
-	assert(res);
+	if (!res) {
+		printf("%s %x\n", "Bad API. EGL error:", eglGetError());
+		goto ExitDisplay;
+	}
 
 	/* create a context for the given config */
 	res = egl_create_context(&data);
-	assert(res);
+	if (!res) {
+		printf("%s %x\n", "Bad context. EGL error:", eglGetError());
+		goto ExitContext;
+	}
 
 	/* create a pbuffer to render to */
 	res = egl_create_surface(&data);
-	assert(res);
+	if (!res) {
+		printf("%s %x\n",
+		       "Failed to create surface. EGL error:", eglGetError());
+		goto ExitContext;
+	}
 
 	/* make the pbuffer the current surface */
 	res = eglMakeCurrent(data.display, data.surface, data.surface,
 			     data.context);
-	assert(res);
+	if (!res) {
+		printf("%s %x\n", "Failed to make surface current. EGL error:",
+		       eglGetError());
+		goto ExitContext;
+	}
 
 	/* create a program to attach shaders to */
 	data.program = glCreateProgram();
+	if (data.program == 0) {
+		printf("%s %x\n",
+		       "Failed to create program. GL error:", glGetError());
+		goto ExitContext;
+	}
 
 	/* setup a vertex shader and register it with the program */
-	GLuint vertex_shader =
-		setup_shader(&data, VERTEX_SHADER, GL_VERTEX_SHADER);
+	res = setup_shader(&data, VERTEX_SHADER, GL_VERTEX_SHADER);
+	if (!res) {
+		printf("%s\n", "Failed to setup vertex shader");
+		goto ExitProgram;
+	}
 
 	/* setup a fragment shader and register it with the program */
-	GLuint fragment_shader =
-		setup_shader(&data, FRAGMENT_SHADER, GL_FRAGMENT_SHADER);
+	res = setup_shader(&data, FRAGMENT_SHADER, GL_FRAGMENT_SHADER);
+	if (!res) {
+		printf("%s\n", "Failed to setup fragment shader");
+		goto ExitProgram;
+	}
 
 	/* bind vPosition to index 0 for our vertex shader */
 	glBindAttribLocation(data.program, 0, "vPosition");
-	assert(glGetError() == GL_NO_ERROR);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to bind attribute. GL error:", glres);
+		goto ExitProgram;
+	}
 
 	/* link the program to test shaders */
 	glLinkProgram(data.program);
-	assert(glGetError() == GL_NO_ERROR);
-
-	/* flag shaders for deletion */
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
+	glres = glGetError();
+	if (glres != GL_NO_ERROR) {
+		printf("%s %x\n", "Failed to link program. GL error:", glres);
+		goto ExitProgram;
+	}
 
 	/* render a frame */
-	draw(&data);
-	assert(glGetError() == GL_NO_ERROR);
+	res = draw(&data);
+	if (!res) {
+		printf("%s\n", "Failed to draw frame");
+		goto ExitProgram;
+	}
+
+	/* send the buffer to the front */
+	res = eglSwapBuffers(data.display, data.surface);
+	if (!res) {
+		printf("%s %x\n", "Failed to swap buffers. EGL error:",
+		       eglGetError());
+		goto ExitProgram;
+	}
 
 	/* dump frame contents to file */
 	dump(&data);
@@ -219,8 +331,11 @@ int32_t main(void)
 	printf("Application dispatched and finished successfully\n");
 
 	/* free stuff */
+ExitProgram:
 	glDeleteProgram(data.program);
+ExitContext:
 	eglDestroyContext(data.display, data.context);
+ExitDisplay:
 	eglTerminate(data.display);
-	return 0;
+	return (glres != GL_NO_ERROR) || (!res);
 }
